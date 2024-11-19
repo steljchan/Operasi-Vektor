8ff1946ec5ef8014eab4328a1e9a846a48029db0
steljchan <stellachandra718@gmail.com>
2024-11-19 12:16:02 +0800

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,5 +1,7 @@
 from Penjumlahan import jumlah
 from Pengurangan import kurang
+from Sudut import Sudut
+from dot import Dotproduct
 x1 = int(input("componen X untuk Vektor 1 : "))
 y1 = int(input("componen y untuk Vektor 1 : "))
 x2 = int(input("componen X untuk Vektor 2 : "))
@@ -22,4 +24,13 @@ if pilih == 1
 if pilih == 2
   D = kurang(A,B)
   print(f"{A} - {B} = {D}")
+if pilih == 3
+  E = 
+if pilih == 4
+  F = Sudut(A,B)
+  print(f"Sudut antara A dan B adalah {F}"
+if pilih == 5
+  G = Dotproduct(A,B)
+  print(f"{A} * {B} = {G}
+if pilih == 6
 
